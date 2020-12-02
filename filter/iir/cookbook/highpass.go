331f7fa0c9aed6d8807e30521bdaeb4cc5ccb002
James Tunnell <jamestunnell@gmail.com>
2020-12-01 18:03:09 -0800

    Add cookbook highpass

--- filter/iir/cookbook/highpass.go
+++ filter/iir/cookbook/highpass.go
@@ -7,30 +7,30 @@ import (
 	"github.com/jamestunnell/go-dsp/filter/iir"
 )
 
-// Lowpass implements a "cookbook" lowpass filter using iir.Biquad,
+// Highpass implements a "cookbook" highpass filter using iir.Biquad,
 // based on the well-known RBJ biquad filter.
-type Lowpass struct {
+type Highpass struct {
 	*iir.Biquad
 	cutoff, q float64
 }
 
-// this Q value works well for lowpass
-const lpQ = 1.0
+// this Q value works well for highpass
+const hpQ = 1.0
 
-// NewLowpass makes a new cookbook lowpass filter.
-func NewLowpass(srate float64) *Lowpass {
-	return &Lowpass{iir.NewBiquad(srate), 0.0, 0.0}
+// NewHighpass makes a new cookbook Highpass filter.
+func NewHighpass(srate float64) *Highpass {
+	return &Highpass{iir.NewBiquad(srate), 0.0, 0.0}
 }
 
-func (lp *Lowpass) Cutoff() float64 {
+func (lp *Highpass) Cutoff() float64 {
 	return lp.cutoff
 }
 
-func (lp *Lowpass) Q() float64 {
+func (lp *Highpass) Q() float64 {
 	return lp.q
 }
 
-func (lp *Lowpass) Configure(cutoff, q float64) error {
+func (lp *Highpass) Configure(cutoff, q float64) error {
 	const twoPi = 2.0 * math.Pi
 
 	srate := lp.Biquad.SampleRate()
@@ -48,9 +48,9 @@ func (lp *Lowpass) Configure(cutoff, q float64) error {
 	alpha := sn / (2.0 * lpQ)
 
 	params := &iir.BiquadParams{
-		B0: (1.0 - cs) / 2.0,
-		B1: 1.0 - cs,
-		B2: (1.0 - cs) / 2.0,
+		B0: (1.0 + cs) / 2.0,
+		B1: -(1.0 + cs),
+		B2: (1.0 + cs) / 2.0,
 		A0: 1.0 + alpha,
 		A1: -2.0 * cs,
 		A2: 1.0 - alpha,
---
+++ filter/iir/cookbook/highpass_test.go
@@ -0,0 +1,24 @@
+package cookbook_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/jamestunnell/go-dsp/filter/iir/cookbook"
+)
+
+func TestHighpass(t *testing.T) {
+	hp := cookbook.NewHighpass(10000.0)
+
+	err := hp.Configure(500.0, 1.0)
+	assert.NoError(t, err)
+
+	// t.Log(gain.LinearToDecibel(hp.MagnitudeResponse(100.0)))
+	// t.Log(gain.LinearToDecibel(hp.MagnitudeResponse(250.0)))
+	// t.Log(gain.LinearToDecibel(hp.MagnitudeResponse(500.0)))
+	// t.Log(gain.LinearToDecibel(hp.MagnitudeResponse(1000.0)))
+	// t.Log(gain.LinearToDecibel(hp.MagnitudeResponse(2500.0)))
+	// t.Log(gain.LinearToDecibel(hp.MagnitudeResponse(4000.0)))
+	// t.Log(gain.LinearToDecibel(hp.MagnitudeResponse(4900.0)))
+}
